a59b7f0151730d39a81f0774122721b1d335f22b
sultan-raja <subaida@zestadz.com>
2011-12-13 11:31:33 +0530

    try

---
+++ test1.php~
@@ -0,0 +1,8 @@
+<?php
+class Reply extends SupportAppModel {
+	var $name = 'Reply';
+	var $tablePrefix = 'support_';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	//test1
+}
+?>
---
+++ test2.php
@@ -0,0 +1,9 @@
+<?php
+class Reply extends SupportAppModel {
+	var $name = 'Reply';
+	var $tablePrefix = 'support_';
+	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	//test2
+
+}
+?>
