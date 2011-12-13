d5c187d9f63c8284c5751f0269877acd800060f5
sultan-raja <subaida@zestadz.com>
2011-12-13 13:10:23 +0530

    Update test2.php

--- test2.php
+++ test2.php
@@ -1,7 +1,7 @@
 <?php
 class Reply extends SupportAppModel {
 	var $name = 'Reply';
-	var $tablePrefix = 'support_';
+	var $tablePrefix = 'support_123';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 	//test
 	//test4
